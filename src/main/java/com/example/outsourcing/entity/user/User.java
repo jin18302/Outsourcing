b6f9b1f6b3ef46a03f33b9cbce9c544947476906
gkdl4239 <gkdl4239@gmail.com>
2025-01-08 11:43:42 +0900

    Feat : user 엔티티 생성 및 관련 메서드 생성

---
+++ src/main/java/com/example/outsourcing/entity/user/User.java
@@ -0,0 +1,44 @@
+package com.example.outsourcing.entity.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, name = "email")
+    private String email;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "is_deleted")
+    private boolean isDeleted = false;
+
+    public void updatePassword(String password) {
+        this.password = password;
+    }
+
+    public void updateName(String name) {
+        this.name = name;
+    }
+
+    public void updateAddress(String address) {
+        this.address = address;
+    }
+
+    public void delete() {
+        this.isDeleted = true;
+    }
+}
