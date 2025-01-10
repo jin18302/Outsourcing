6265165036f9101d9e97ec20141b24a44b5b5ded
gkdl4239 <gkdl4239@gmail.com>
2025-01-11 02:14:50 +0900

    Fix : 예외처리 커스텀으로 변경

--- src/main/java/com/example/outsourcing/repository/menu/MenuConnector.java
+++ src/main/java/com/example/outsourcing/repository/menu/MenuConnector.java
@@ -1,31 +1,27 @@
 package com.example.outsourcing.repository.menu;
 
+import com.example.outsourcing.common.exception.InvalidRequestException;
 import com.example.outsourcing.entity.Menu;
 import com.example.outsourcing.service.menu.MenuConnectorInterface;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class MenuConnector implements MenuConnectorInterface {
-
     private final MenuRepository menuRepository;
 
-    @Override
     public Menu save(Menu menu) {
         return menuRepository.save(menu);
     }
 
-    @Override
     public Menu findById(Long id) {
-        return menuRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST,"해당 메뉴는 존재하지 않습니다"));
+        return menuRepository.findById(id)
+                .orElseThrow(() -> new InvalidRequestException("해당 메뉴는 존재하지 않습니다"));
     }
 
-    @Override
     public List<Menu> findByStoreId(Long storeId) {
         return menuRepository.findByStoreId(storeId);
     }
