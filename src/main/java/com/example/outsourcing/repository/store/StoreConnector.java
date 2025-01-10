ce3c980432eaa2851ce44d3c1e1b05ba9283f680
gkdl4239 <gkdl4239@gmail.com>
2025-01-11 02:22:32 +0900

    Fix : 인터페이스 메서드 전부 상속

--- src/main/java/com/example/outsourcing/repository/store/StoreConnector.java
+++ src/main/java/com/example/outsourcing/repository/store/StoreConnector.java
@@ -1,19 +1,56 @@
 package com.example.outsourcing.repository.store;
 
+import com.example.outsourcing.common.exception.NotFoundException;
 import com.example.outsourcing.entity.Store;
+import com.example.outsourcing.service.store.StoreConnectorInterface;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
-import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
-public class StoreConnector {
+public class StoreConnector implements StoreConnectorInterface{
 
     private final StoreRepository storeRepository;
 
+    @Override
     public Store findById(Long storeId) {
         return storeRepository.findByIdAndIsDeletedFalse(storeId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"가게없음"));
+                .orElseThrow(() -> new NotFoundException("가게없음"));
     }
+
+    @Override
+    public List<Store> findMyStoresByUserId(Long userId) {
+        return storeRepository.findMyStoresByUserId(userId);
+    }
+
+    @Override
+    public int countByUserId(Long userId) {
+        return storeRepository.countByUserId(userId);
+    }
+
+    @Override
+    public Store save(Store store) {
+        return storeRepository.save(store);
+    }
+
+    @Override
+    public Page<Store> findByIsDeletedFalse(Pageable pageable) {
+        return storeRepository.findByIsDeletedFalse(pageable);
+    }
+
+    @Override
+    public Page<Store> findAllByName(String name, Pageable pageable) {
+        return storeRepository.findAllByName(name, pageable);
+    }
+
+    @Override
+    public Optional<Object> findByIdAndIsDeletedFalse(Long storeId) {
+        return Optional.empty();
+    }
+
 }
