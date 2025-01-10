03e1bf746e6c1c35faea5e162c7c9efb20e6b63c
gkdl4239 <gkdl4239@gmail.com>
2025-01-10 11:24:25 +0900

    Fix : Dto 일반 클래스에서 레코드로 변경

--- src/main/java/com/example/outsourcing/dto/user/response/UserResponse.java
+++ src/main/java/com/example/outsourcing/dto/user/response/UserResponse.java
@@ -1,14 +1,8 @@
 package com.example.outsourcing.dto.user.response;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
-public class UserResponse {
-
-    private final Long id;
-    private final String email;
-    private final String name;
+public record UserResponse(
+        Long id,
+        String email,
+        String name) {
 
 }
