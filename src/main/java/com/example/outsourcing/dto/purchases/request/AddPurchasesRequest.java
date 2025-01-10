728dda783d2d29b2a44b464cf8194b2ad5ef37e7
gkdl4239 <gkdl4239@gmail.com>
2025-01-11 02:08:33 +0900

    Fix : userId 토큰에서 받게 수정

--- src/main/java/com/example/outsourcing/dto/purchases/request/AddPurchasesRequest.java
+++ src/main/java/com/example/outsourcing/dto/purchases/request/AddPurchasesRequest.java
@@ -3,8 +3,8 @@ package com.example.outsourcing.dto.purchases.request;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record AddPurchasesRequest(@JsonProperty("storeId")Long storeId,
-                                  @JsonProperty("menuId")Long menuId,
-                                  @JsonProperty("userId")Long userId) {
+                                  @JsonProperty("menuId")Long menuId
+                                  ) {
 
 
 }
