541bf394fafa67980c98dfe9ea1054032becc123
gkdl4239 <gkdl4239@gmail.com>
2025-01-11 20:21:18 +0900

    Fix : 상위 필터에서의 처리를 위한 예외 던지기

--- src/main/java/com/example/outsourcing/config/JwtFilter.java
+++ src/main/java/com/example/outsourcing/config/JwtFilter.java
@@ -1,6 +1,6 @@
 package com.example.outsourcing.config;
 
-import com.example.outsourcing.common.status.UserRole;
+import com.example.outsourcing.common.exception.UnauthorizedException;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.MalformedJwtException;
@@ -8,10 +8,11 @@ import io.jsonwebtoken.UnsupportedJwtException;
 import jakarta.servlet.FilterConfig;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 import java.io.IOException;
+import java.io.InvalidClassException;
 
 
 @Slf4j
@@ -28,7 +29,6 @@ public class JwtFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
-        HttpServletResponse httpResponse = (HttpServletResponse) response;
 
         String url = httpRequest.getRequestURI();
 
@@ -40,8 +40,7 @@ public class JwtFilter implements Filter {
         String bearerJwt = httpRequest.getHeader("Authorization");
 
         if (bearerJwt == null) {
-            httpResponse.sendError(HttpServletResponse.SC_BAD_REQUEST, "JWT 토큰이 필요합니다.");
-            return;
+            throw new UnauthorizedException("로그인이 필요합니다.");
         }
 
         String jwt = jwtUtil.substringToken(bearerJwt);
@@ -49,8 +48,7 @@ public class JwtFilter implements Filter {
         try {
             Claims claims = jwtUtil.extractClaims(jwt);
             if (claims == null) {
-                httpResponse.sendError(HttpServletResponse.SC_BAD_REQUEST, "잘못된 JWT 토큰입니다.");
-                return;
+                throw new InvalidClassException("잘못된 JWT 토큰입니다.");
             }
 
             httpRequest.setAttribute("userId", Long.parseLong(claims.getSubject()));
@@ -59,14 +57,11 @@ public class JwtFilter implements Filter {
 
             chain.doFilter(request, response);
         } catch (SecurityException | MalformedJwtException e) {
-            log.error("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.", e);
-            httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "유효하지 않는 JWT 서명입니다.");
+            throw new UnauthorizedException("유효하지 않는 JWT 서명입니다.");
         } catch (ExpiredJwtException e) {
-            log.error("Expired JWT token, 만료된 JWT token 입니다.", e);
-            httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "만료된 JWT 토큰입니다.");
+            throw new ExpiredJwtException(null, null, "로그인 해주세요");
         } catch (UnsupportedJwtException e) {
-            log.error("Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다.", e);
-            httpResponse.sendError(HttpServletResponse.SC_BAD_REQUEST, "지원되지 않는 JWT 토큰입니다.");
+            throw new UnsupportedJwtException("지원되지 않는 JWT 토큰입니다.");
         }
     }
 
