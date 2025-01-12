749d1273c0fbb29ca7b42e43c031e2f09321f4cb
gkdl4239 <gkdl4239@gmail.com>
2025-01-12 16:12:12 +0900

    Fix : 필터에서 예외 처리시 타임스탬프 추가

--- src/main/java/com/example/outsourcing/config/ExceptionHandlerFilter.java
+++ src/main/java/com/example/outsourcing/config/ExceptionHandlerFilter.java
@@ -3,25 +3,29 @@ package com.example.outsourcing.config;
 import com.example.outsourcing.common.exception.InvalidRequestException;
 import com.example.outsourcing.common.exception.UnauthorizedException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.UnsupportedJwtException;
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ExceptionHandlerFilter extends OncePerRequestFilter {
+public class            ExceptionHandlerFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(
             HttpServletRequest request,
             HttpServletResponse response,
             FilterChain chain
-    ) throws IOException {
+    ) throws IOException, ServletException {
 
         try {
             chain.doFilter(request, response);
@@ -29,8 +33,6 @@ public class ExceptionHandlerFilter extends OncePerRequestFilter {
             handleException(response, HttpStatus.UNAUTHORIZED, e.getMessage());
         } catch (InvalidRequestException | UnsupportedJwtException e) {
             handleException(response, HttpStatus.BAD_REQUEST, e.getMessage());
-        } catch (Exception e) {
-            handleException(response, HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 에러가 발생했습니다.");
         }
     }
 
@@ -40,13 +42,16 @@ public class ExceptionHandlerFilter extends OncePerRequestFilter {
         response.setCharacterEncoding("UTF-8");
 
         Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("timestamp", LocalDateTime.now());
         errorResponse.put("code", status.value());
         errorResponse.put("message", message);
         errorResponse.put("status", status.name());
 
         ObjectMapper objectMapper = new ObjectMapper();
-        String jsonResponse = objectMapper.writeValueAsString(errorResponse);
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
+        String jsonResponse = objectMapper.writeValueAsString(errorResponse);
         response.getWriter().write(jsonResponse);
         response.getWriter().flush();
         response.getWriter().close();
