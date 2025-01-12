f7ab2df52cd96828207100eeb971bc26050606c0
gkdl4239 <gkdl4239@gmail.com>
2025-01-12 19:05:15 +0900

    Feat : 유저별 주문 조회 가게별 주문 조회 메서드 추가

--- src/main/java/com/example/outsourcing/controller/purchases/PurchasesController.java
+++ src/main/java/com/example/outsourcing/controller/purchases/PurchasesController.java
@@ -5,10 +5,13 @@ import com.example.outsourcing.dto.purchases.request.AddPurchasesRequest;
 import com.example.outsourcing.dto.purchases.request.UpdatePurchasesRequest;
 import com.example.outsourcing.dto.purchases.response.PurchasesResponse;
 import com.example.outsourcing.service.purchases.PurchasesService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/purchases")
@@ -21,8 +24,26 @@ public class PurchasesController {
     @PostMapping
     public ResponseEntity<PurchasesResponse> createPurchases(
             @RequestAttribute("userId") Long userId,
-            @RequestBody AddPurchasesRequest request) {
+            @Valid @RequestBody AddPurchasesRequest request) {
         PurchasesResponse purchases = purchasesService.createPurchases(request, userId);
+        return ResponseEntity.status(HttpStatus.OK).body(purchases);
+    }
+
+    @RequireRole("OWNER")
+    @GetMapping("/stores/{storeId}")
+    public ResponseEntity<List<PurchasesResponse>> getStorePurchases(
+            @RequestAttribute("userId") Long userId,
+            @PathVariable Long storeId
+    ) {
+        List<PurchasesResponse> purchases = purchasesService.getStorePurchases(userId, storeId);
+        return ResponseEntity.status(HttpStatus.OK).body(purchases);
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<PurchasesResponse>> getUserPurchases(
+            @RequestAttribute("userId") Long userId
+    ) {
+        List<PurchasesResponse> purchases = purchasesService.getUserPurchases(userId);
         return ResponseEntity.status(HttpStatus.OK).body(purchases);
     }
 
@@ -42,7 +63,7 @@ public class PurchasesController {
     @PatchMapping
     public ResponseEntity<PurchasesResponse> changePurchasesByOwner(
             @RequestAttribute("userId") Long userId,
-            @RequestBody UpdatePurchasesRequest request) {
+            @Valid @RequestBody UpdatePurchasesRequest request) {
 
         PurchasesResponse purchases =  purchasesService.changePurchasesByOwner(userId, request);
 
